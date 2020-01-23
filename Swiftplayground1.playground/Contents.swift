e5fb96378469e9fbfd62437ab2f6422b3e33554a
CM Student <cmstudent@UHWO-ACM-11.local>
2020-01-23 10:44:00 -1000

    Completed part 6

---
+++ Swiftplayground1.playground/Contents.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//var myFirstVariable:String
+
+
+
+var myFirstVariable = "i've created my first string"
+
+
+
+//declare and assign variable in single line of code
+
+var mySecondVariable = "and look I added two strings together"
+
+
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+
+
+
+
+print(myThirdVariable)
+
+
+
+//var myFirstVariable : Dog
+
+
+
+var myFirstPack = "i've creatred my first dog"
+
+//declare and assign variable in single line of code
+
+var mySecondPack = "I like walk my dog"
+
+
+
+var myThirdpack = "the dog is tired of walking"
+
+
+
+var myFourthpack = myFirstPack + mySecondPack + myThirdpack
+
+
+
+print(myFourthpack)
+
+//Assigment #2 True or False
+var appState = false
+print(appState)
+
+var numberOne = 5
+
+var numberTwo = 2
+
+var comparison = numberOne < numberTwo
+
+
+var placeOne = 2
+
+var placeTwo = 4
+
+var Comparison = placeOne < placeTwo
+
+
+
+
+
+
+
